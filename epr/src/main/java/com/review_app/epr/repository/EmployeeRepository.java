9e72166f027da747e9ac97a5d1d72824cce21d50
monicapopuri1 <100665759+monicapopuri1@users.noreply.github.com>
2025-12-17 16:57:41 +0530

    Update EmployeeRepository.java

--- epr/src/main/java/com/review_app/epr/repository/EmployeeRepository.java
+++ epr/src/main/java/com/review_app/epr/repository/EmployeeRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    // Add custom methods here later, like finding by email
+    
 }

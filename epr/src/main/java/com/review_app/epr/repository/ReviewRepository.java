e3f2c259c8bc07fbe7b5b10abd680faaa2421e0d
monicapopuri1 <100665759+monicapopuri1@users.noreply.github.com>
2025-12-17 16:58:05 +0530

    Update ReviewRepository.java

--- epr/src/main/java/com/review_app/epr/repository/ReviewRepository.java
+++ epr/src/main/java/com/review_app/epr/repository/ReviewRepository.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-// JpaRepository requires two arguments: the Entity class and the type of its Primary Key (ID)
 public interface ReviewRepository extends JpaRepository<Review, Long> {
 
     // Spring Data JPA automatically provides methods like save(), findAll(), findById(), etc.
